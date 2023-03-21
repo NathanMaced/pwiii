875da2c4dbdf93355a7695d624faf6e04fe61c31
NathanMaced <nathanmacedo02@gmail.com>
2023-03-20 22:12:22 -0300

    Criando conta corrente

--- ContaCorrente.php
+++ ContaCorrente.php
@@ -7,6 +7,19 @@ class ContaCorrente extends Conta {
         parent:: __construct($agencia, $codigo, $dataDeCriacao, $titular, $senha, $saldo);
         $this->limite = $limite;
     }
+    function retirar($quantia){
+        $cpmf = 0.05;
+
+        if(($this->saldo = $this->limite)>=$quantia){
+            parent::retirar($quantia);
+            parent::retirar($quantia * $cpmf);
+        }
+        else{
+            echo "<br>Retirada não permitida ...<br>";
+            return false;
+        }
+        return true;
+    }
 }
 
 ?>
