ba2b40fb25e09dd051592304c8cebf0dd8164ccc
NathanMaced <nathanmacedo02@gmail.com>
2023-03-22 21:56:46 -0300

    Criando o objeto

---
+++ Objeto.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once 'Pessoa.php';
+include_once 'Conta.php';
+
+$carlos = new Pessoa;
+$carlos->Codigo = 10;
+$carlos->Nome = "Carlos da Silva";
+$carlos->ALtura = 1.85;
+$carlos->Idade = 25;
+$carlos->Nascimento = '10/04/1976';
+$carlos->Escolaridade = "Ensino Médio";
+
+echo "Manipulando o objeto $carlos->Nome <br>";
+
+?>
